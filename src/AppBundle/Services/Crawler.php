bd25af0322e612dd33ceaad08c5905525bd52275
Vladimir Falushko <vov8278@gmail.com>
2017-05-18 00:50:25 +0300

    Get all website pages added

--- src/AppBundle/Services/Crawler.php
+++ src/AppBundle/Services/Crawler.php
@@ -2,8 +2,10 @@
 
 namespace AppBundle\Services;
 
+use ArrayIterator;
 use GuzzleHttp\Client as HTTPClient;
 use PHPHtmlParser\Dom;
+use SimpleXMLElement;
 
 /**
  * Moves through website, finds all pages and outbound links. Checks http response status codes.
@@ -19,6 +21,9 @@ class Crawler
      */
     public function crawl(string $website) : array
     {
+
+        $this->getAllWebsitePages($website);
+
         $result = [];
 
         $dom = new Dom;
@@ -40,7 +45,78 @@ class Crawler
      */
     public function getAllWebsitePages(string $website) : array
     {
-        // todo
+        $pages = [];
+
+        $client = new HTTPClient();
+        $response = $client->get($website . '/sitemhap.xml', ['exceptions' => false]);
+
+        if ($response->getStatusCode() === 200) {
+            $links = new SimpleXMLElement($response->getBody());
+
+            foreach ($links as $link) $pages[] = (string) $link->loc;
+
+            dump($pages, '1'); exit();
+        } else {
+
+            $pages[] = $website;
+            $counter = 0;
+
+            while (true) {
+                if ($counter >= count($pages)) break;
+
+                $dom = new Dom;
+                $dom->load($pages[$counter]);
+                $links = $dom->find('a');
+
+                foreach ($links as $link) {
+                    $link = $link->tag->getAttribute('href')['value'];
+
+                    if ($this->isLinkOutbound($link, $website)) continue;
+                    if ($this->isLinkToMedia($link)) continue;
+                    if (in_array($link, $pages)) continue;
+
+                    $pages[] = $link;
+                }
+
+                $counter++;
+            }
+
+
+            dump($pages, '2'); exit();
+
+        }
+
+//
+//        $arr = array();
+//        $arr['b'] = 'book';
+//
+//        $array_iterator = new ArrayIterator($arr);
+//
+//
+//        foreach($array_iterator as $key=>$val) {
+//            print "key=>$key\n";
+//            if(!isset($array_iterator['a']))
+//                $array_iterator['a'] = 'apple';
+//        }
+
+
+
+        return $pages;
+    }
+
+    /**
+     * Get all inbound links from page.
+     * @param string $page
+     * @param string $website
+     * @return array
+     */
+    public function getAllInboundLinksFromPage(string $page, string $website) : array
+    {
+        $result = [];
+
+
+
+        return $result;
     }
 
     /**
@@ -53,6 +129,24 @@ class Crawler
     {
         return (strpos($link, $website) !== false) ? false : true;
     }
+
+    /**
+     * Get response status for link.
+     * @param string $link
+     * @return array
+     */
+    public function getHTTPResponseStatus(string $link) : array
+    {
+        $client = new HTTPClient();
+        $response = $client->request('GET', $link);
+
+        return [
+            'code' => $response->getStatusCode(),
+            'phrase' => $response->getReasonPhrase()
+        ];
+    }
+
+    //todo add broken images checker
 
     /**
      * Check if link is picture, video or mp3.
@@ -81,21 +175,5 @@ class Crawler
     public function isMediaDoesNotExist(string $link) : bool
     {
         return $this->getHTTPResponseStatus($link)['code'] !== 200 ? true : false;
-    }
-
-    /**
-     * Get response status for link.
-     * @param string $link
-     * @return array
-     */
-    public function getHTTPResponseStatus(string $link) : array
-    {
-        $client = new HTTPClient();
-        $response = $client->request('GET', $link);
-
-        return [
-            'code' => $response->getStatusCode(),
-            'phrase' => $response->getReasonPhrase()
-        ];
     }
 }
